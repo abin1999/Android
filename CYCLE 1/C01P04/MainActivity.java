623be92653d44cf1fc831500b567d73cc5d0d46e
abin1999 <74650927+abin1999@users.noreply.github.com>
2021-12-07 10:56:57 +0530

    Add files via upload

---
+++ CYCLE 1/C01P04/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sjcet.program4;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Click(View view) {
+        LinearLayout layout1;
+        layout1 = findViewById(R.id.ll1);
+        layout1.setBackgroundColor(Color.RED);
+    }
+}
